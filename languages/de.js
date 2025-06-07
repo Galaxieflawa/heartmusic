41e7a08f201c5ad6248ebb502dbbb6000a47ff8b
Galaxieflawa <galaxieflawa@gmail.com>
2025-06-07 13:01:45 +0200

    Update de.js

--- languages/de.js
+++ languages/de.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer: "Entwickelt von SSRR | Prime Music v1.2",
+    footer: "Entwickelt von GalaxieFLawa ",
     ping: {
       description: "Prüfe die Bot-Latenz",
       response: "Ping wird gemessen...",
